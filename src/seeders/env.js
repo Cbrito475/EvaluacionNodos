4c6668f0a733f18b2f12c0aa14750c40699bbc8a
Cbrito475 <90299331+Cbrito475@users.noreply.github.com>
2025-05-05 19:26:58 -0400

    Merge remote changes

--- src/seeders/env.js
+++ src/seeders/env.js
@@ -1,3 +1,2 @@
-const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/nodedb';
-
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/nodedb'|| 'mongodb://mongo:RJocOyTeYOvTRmPIYwFYLwJrkMUtcQuV@interchange.proxy.rlwy.net:12746'
 export { MONGO_URI };
